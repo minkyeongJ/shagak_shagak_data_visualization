615f5c8151b5e1fe4323c8e6e5903c77cd4aaf1d
JiHyeon Lee <justjh30@gmail.com>
2024-09-10 21:38:03 +0900

    [Jihyeon/leaflet] 지도 기본 페이지 생성 및 일부 내용 추가 (#1)
    * 이전 작업 가져오기
    
    * 라우터 설정 완료
    
    * 커스텀 마커 아이콘 교체 버그 수정
    
    * 커스텀 마커 아이콘 변경 완료
    
    * 엑셀 데이터 가져와서 미리보기 구현
    
    * 아이콘 변경 시 마커 클릭 오류 해결
    
    * 마커에 툴팁 추가
    
    * 벡터 그리기 완료
    
    * 벡터에 스타일 적용하기 완료

--- src/Router.jsx
+++ src/Router.jsx
@@ -17,6 +17,18 @@ import PieChart from "./pages/chartjs/PieChart";
 import PolarAreaChart from "./pages/chartjs/PolarAreaChart";
 import RadarChart from "./pages/chartjs/RadarChart";
 import ScatterChart from "./pages/chartjs/ScatterChart";
+import Map01 from "./pages/map/01";
+import Map0101 from "./pages/map/01/01";
+import Map0102 from "./pages/map/01/02";
+import Map0103 from "./pages/map/01/03";
+import Map0201 from "./pages/map/02/01";
+import Map0202 from "./pages/map/02/02";
+import Map0203 from "./pages/map/02/03";
+import Map0301 from "./pages/map/03/01";
+import Map0302 from "./pages/map/03/02";
+import Map03 from "./pages/map/03";
+import Map02 from "./pages/map/02";
+import Map04 from "./pages/map/04";
 
 const router = createBrowserRouter([
   {
@@ -95,7 +107,38 @@ const router = createBrowserRouter([
       {
         path: "map",
         element: <Map />,
-        // children: [{}],
+        children: [
+          {
+            path: "01 Map",
+            element: <Map01 />,
+            children: [
+              { path: "MapContainer", element: <Map0101 /> },
+              { path: "TileLayer", element: <Map0102 /> },
+              { path: "Custom Hooks", element: <Map0103 /> },
+            ],
+          },
+          {
+            path: "02 marker",
+            element: <Map02 />,
+            children: [
+              { path: "Marker", element: <Map0201 /> },
+              { path: "Popup & Tooltip", element: <Map0202 /> },
+              { path: "Custom Marker", element: <Map0203 /> },
+            ],
+          },
+          {
+            path: "03 Vector",
+            element: <Map03 />,
+            children: [
+              { path: "Vector", element: <Map0301 /> },
+              { path: "2", element: <Map0302 /> },
+            ],
+          },
+          {
+            path: "map with CSV",
+            element: <Map04 />,
+          },
+        ],
       },
     ],
   },
